AdrianDC/aosp_development_sony8960_q

WifiMetrics: add metric for alert reasons
Add metric to track the number of alerts triggered
by Wifi driver/firmware. This will give us a
concrete measurement of stability in the field.

BUG=29158746
TEST=tests/wifitests/runtests.sh

Change-Id: I435e5d792a97c197fe685e5b2c05d7b8efda62f0

service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3930,7 +3930,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                 case CMD_FIRMWARE_ALERT:
                     if (mWifiDiagnostics != null) {
                         byte[] buffer = (byte[])message.obj;
-                        mWifiDiagnostics.captureAlertData(message.arg1, buffer);
+                        int alertReason = message.arg1;
+                        mWifiDiagnostics.captureAlertData(alertReason, buffer);
+                        mWifiMetrics.incrementAlertReasonCount(alertReason);
                     }
                     break;
                 case CMD_GET_LINK_LAYER_STATS:

phhusson/platform_packages_modules_Wifi

service/java/com/android/server/wifi/WifiStateMachine.java
@@ -4189,7 +4189,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                 case CMD_FIRMWARE_ALERT:
                     if (mWifiLogger != null) {
                         byte[] buffer = (byte[])message.obj;
-                        mWifiLogger.captureAlertData(message.arg1, buffer);
+                        int alertReason = message.arg1;
+                        mWifiLogger.captureAlertData(alertReason, buffer);
+                        mWifiMetrics.incrementAlertReasonCount(alertReason);
                     }
                     break;
                 case CMD_GET_LINK_LAYER_STATS: