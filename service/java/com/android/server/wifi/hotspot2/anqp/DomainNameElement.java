Dreams-Nougat/platform_frameworks_opt_net_wifi

Cleanup support for parsing various length integer and string from ByteBuffer
Move and cleanup the parsing functions from
com.android.server.wifi.hotspot2.anqp.Constants to the newly created
ByteBufferReader, since these are generic parsing functions (not
specific to ANQP).

An unchecked runtime exception will be thrown when an error is
encountered.  The possible exceptions are documented for each
API.  It is the caller's responsibility to handle those
exceptions appropriately (e.g. propagate the exceptions upwards
with appropriate documentation or catch the exception).

The handling of unchecked runtime exceptions for ANQP elements
parsing will be added in the follow-on CL as part of the ANQP cleanup.

b/33296974 is filed for tracking handling of unchecked runtime
exceptions for parsing information elements.

Bug: 33000864
Test: frameworks/opt/net/wifi/tests/wifitests/runtests.sh
Change-Id: I6964520e7cc86071b2096408d6cb0503e0f618ff

service/java/com/android/server/wifi/hotspot2/anqp/DomainNameElement.java
@@ -1,5 +1,7 @@
 package com.android.server.wifi.hotspot2.anqp;
 
+import com.android.server.wifi.ByteBufferReader;
+
 import java.net.ProtocolException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -20,7 +22,8 @@ public class DomainNameElement extends ANQPElement {
 
         while (payload.hasRemaining()) {
             // Use latin-1 to decode for now - safe for ASCII and retains encoding
-            mDomains.add(Constants.getPrefixedString(payload, 1, StandardCharsets.ISO_8859_1));
+            mDomains.add(ByteBufferReader.readByteLengthAndString(
+                    payload, StandardCharsets.ISO_8859_1));
         }
     }
 

service/java/com/android/server/wifi/hotspot2/anqp/IconInfo.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 
 import static com.android.server.wifi.hotspot2.anqp.Constants.SHORT_MASK;
 
+import com.android.server.wifi.ByteBufferReader;
+
 /**
  * The Icons available OSU Providers sub field, as specified in
  * Wi-Fi Alliance Hotspot 2.0 (Release 2) Technical Specification - Version 5.00,
@@ -26,10 +28,10 @@ public class IconInfo {
 
         mWidth = payload.getShort() & SHORT_MASK;
         mHeight = payload.getShort() & SHORT_MASK;
-        mLanguage = Constants.getTrimmedString(payload,
-                Constants.LANG_CODE_LENGTH, StandardCharsets.US_ASCII);
-        mIconType = Constants.getPrefixedString(payload, 1, StandardCharsets.US_ASCII);
-        mFileName = Constants.getPrefixedString(payload, 1, StandardCharsets.UTF_8);
+        mLanguage = ByteBufferReader.readString(
+                payload, Constants.LANG_CODE_LENGTH, StandardCharsets.US_ASCII).trim();
+        mIconType = ByteBufferReader.readByteLengthAndString(payload, StandardCharsets.US_ASCII);
+        mFileName = ByteBufferReader.readByteLengthAndString(payload, StandardCharsets.UTF_8);
     }
 
     public int getWidth() {

service/java/com/android/server/wifi/hotspot2/anqp/NAIRealmData.java
@@ -1,6 +1,7 @@
 package com.android.server.wifi.hotspot2.anqp;
 
 import com.android.server.wifi.hotspot2.anqp.eap.EAPMethod;
+import com.android.server.wifi.ByteBufferReader;
 import com.android.server.wifi.hotspot2.AuthMatch;
 import com.android.server.wifi.hotspot2.DomainMatcher;
 import com.android.server.wifi.hotspot2.Utils;
@@ -30,9 +31,8 @@ public class NAIRealmData {
         }
         boolean utf8 = (payload.get() & 1) == Constants.UTF8_INDICATOR;
 
-        String realm = Constants.getPrefixedString(payload, 1, utf8 ?
-                StandardCharsets.UTF_8 :
-                StandardCharsets.US_ASCII);
+        String realm = ByteBufferReader.readByteLengthAndString(
+                payload, utf8 ? StandardCharsets.UTF_8 : StandardCharsets.US_ASCII);
         String[] realms = realm.split(";");
         mRealms = new ArrayList<>();
         for (String realmElement : realms) {

phhusson/platform_packages_modules_Wifi

service/java/com/android/server/wifi/hotspot2/anqp/DomainNameElement.java
@@ -1,5 +1,7 @@
 package com.android.server.wifi.hotspot2.anqp;
 
+import com.android.server.wifi.ByteBufferReader;
+
 import java.net.ProtocolException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -20,7 +22,8 @@ public class DomainNameElement extends ANQPElement {
 
         while (payload.hasRemaining()) {
             // Use latin-1 to decode for now - safe for ASCII and retains encoding
-            mDomains.add(Constants.getPrefixedString(payload, 1, StandardCharsets.ISO_8859_1));
+            mDomains.add(ByteBufferReader.readStringWithByteLength(
+                    payload, StandardCharsets.ISO_8859_1));
         }
     }
 

service/java/com/android/server/wifi/hotspot2/anqp/IconInfo.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 
 import static com.android.server.wifi.hotspot2.anqp.Constants.SHORT_MASK;
 
+import com.android.server.wifi.ByteBufferReader;
+
 /**
  * The Icons available OSU Providers sub field, as specified in
  * Wi-Fi Alliance Hotspot 2.0 (Release 2) Technical Specification - Version 5.00,
@@ -26,10 +28,10 @@ public class IconInfo {
 
         mWidth = payload.getShort() & SHORT_MASK;
         mHeight = payload.getShort() & SHORT_MASK;
-        mLanguage = Constants.getTrimmedString(payload,
-                Constants.LANG_CODE_LENGTH, StandardCharsets.US_ASCII);
-        mIconType = Constants.getPrefixedString(payload, 1, StandardCharsets.US_ASCII);
-        mFileName = Constants.getPrefixedString(payload, 1, StandardCharsets.UTF_8);
+        mLanguage = ByteBufferReader.readString(
+                payload, Constants.LANG_CODE_LENGTH, StandardCharsets.US_ASCII).trim();
+        mIconType = ByteBufferReader.readStringWithByteLength(payload, StandardCharsets.US_ASCII);
+        mFileName = ByteBufferReader.readStringWithByteLength(payload, StandardCharsets.UTF_8);
     }
 
     public int getWidth() {

service/java/com/android/server/wifi/hotspot2/anqp/NAIRealmData.java
@@ -1,6 +1,7 @@
 package com.android.server.wifi.hotspot2.anqp;
 
 import com.android.server.wifi.hotspot2.anqp.eap.EAPMethod;
+import com.android.server.wifi.ByteBufferReader;
 import com.android.server.wifi.hotspot2.AuthMatch;
 import com.android.server.wifi.hotspot2.DomainMatcher;
 import com.android.server.wifi.hotspot2.Utils;
@@ -30,9 +31,8 @@ public class NAIRealmData {
         }
         boolean utf8 = (payload.get() & 1) == Constants.UTF8_INDICATOR;
 
-        String realm = Constants.getPrefixedString(payload, 1, utf8 ?
-                StandardCharsets.UTF_8 :
-                StandardCharsets.US_ASCII);
+        String realm = ByteBufferReader.readStringWithByteLength(
+                payload, utf8 ? StandardCharsets.UTF_8 : StandardCharsets.US_ASCII);
         String[] realms = realm.split(";");
         mRealms = new ArrayList<>();
         for (String realmElement : realms) {