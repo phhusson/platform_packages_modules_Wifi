3edab56e567f0381c79c818163d14504597bb5bc
Chalard Jean <jchalard@google.com>
2021-01-27 20:45:53 +0900

    Allow the modern way to get ConnectivityManager
    This removes a number of checks that no more interactions
    were done on ConnectivityManager because this is
    overspecified considering the reliance on third-party
    libraries like NetworkFactory. This is exemplified in the
    failures happening if these checks are not removed ;
    methods like registerNetworkProvider used to be called
    by NetworkFactory, but the old code would not see them
    because it would not mock the instances of CM obtained
    through the modern method.
    
    Upcoming changes to NetworkFactory will also reshuffle
    a little bit what hidden methods of ConnectivityManager
    will call, so amending the changes to test for these
    calls to the internals of ConnectivityManager is also
    overspecifying.
    
    Bug: None
    Test: FrameworksWifiTests
    Change-Id: I18683c28bf05928a0ce5ddaf44671385b8d9e7f0
    Merged-In: I18683c28bf05928a0ce5ddaf44671385b8d9e7f0

--- service/tests/wifitests/src/com/android/server/wifi/WifiNetworkFactoryTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/WifiNetworkFactoryTest.java
@@ -179,6 +179,8 @@ public class WifiNetworkFactoryTest extends WifiBaseTest {
         when(mContext.getPackageManager()).thenReturn(mPackageManager);
         when(mContext.getSystemService(eq(Context.CONNECTIVITY_SERVICE)))
                 .thenReturn(mConnectivityManager);
+        when(mContext.getSystemService(eq(ConnectivityManager.class)))
+                .thenReturn(mConnectivityManager);
         when(mContext.getSystemService(CompanionDeviceManager.class))
                 .thenReturn(mCompanionDeviceManager);
         when(mPackageManager.getNameForUid(TEST_UID_1)).thenReturn(TEST_PACKAGE_NAME_1);
