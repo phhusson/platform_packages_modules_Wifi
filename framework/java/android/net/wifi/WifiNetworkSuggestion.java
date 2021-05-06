5aea7064d3adf18a00b7c6a7e2666c33cf6ca9d7
Nate Jiang <qiangjiang@google.com>
2021-05-06 13:48:47 -0700

    Add RequiresApi Annotations to S API
    Bug: 181358771
    Test: atest FrameworksWifiApiTests, FrameworksWifiTests
    Change-Id: Ia3aeb75f4300bf2b920393767e9078dfb456f1b1

--- framework/java/android/net/wifi/WifiNetworkSuggestion.java
+++ framework/java/android/net/wifi/WifiNetworkSuggestion.java
@@ -28,12 +28,15 @@ import android.net.MacAddress;
 import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
 import android.net.wifi.hotspot2.PasspointConfiguration;
+import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+
+import androidx.annotation.RequiresApi;
 
 import com.android.modules.utils.build.SdkLevel;
 
@@ -512,6 +515,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
          * @return Instance of {@link Builder} to enable chaining of the builder method.
          * @throws IllegalArgumentException if subscriptionId equals to {@link SubscriptionManager#INVALID_SUBSCRIPTION_ID}
          */
+        @RequiresApi(Build.VERSION_CODES.S)
         public @NonNull Builder setSubscriptionId(int subscriptionId) {
             if (!SdkLevel.isAtLeastS()) {
                 throw new UnsupportedOperationException();
@@ -548,9 +552,6 @@ public final class WifiNetworkSuggestion implements Parcelable {
          * @return Instance of {@link Builder} to enable chaining of the builder method.
          */
         public @NonNull Builder setPriorityGroup(@IntRange(from = 0) int priorityGroup) {
-            if (!SdkLevel.isAtLeastS()) {
-                throw new UnsupportedOperationException();
-            }
             mPriorityGroup = priorityGroup;
             return this;
         }
@@ -801,6 +802,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
          * @hide
          */
         @SystemApi
+        @RequiresApi(Build.VERSION_CODES.S)
         public @NonNull Builder setOemPaid(boolean isOemPaid) {
             if (!SdkLevel.isAtLeastS()) {
                 throw new UnsupportedOperationException();
@@ -843,6 +845,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
          * @hide
          */
         @SystemApi
+        @RequiresApi(Build.VERSION_CODES.S)
         public @NonNull Builder setOemPrivate(boolean isOemPrivate) {
             if (!SdkLevel.isAtLeastS()) {
                 throw new UnsupportedOperationException();
@@ -875,6 +878,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
          *                               merged network (if true) or non-merged network (if false);
          * @return Instance of {@link Builder} to enable chaining of the builder method.
          */
+        @RequiresApi(Build.VERSION_CODES.S)
         public @NonNull Builder setCarrierMerged(boolean isCarrierMerged) {
             if (!SdkLevel.isAtLeastS()) {
                 throw new UnsupportedOperationException();
@@ -1436,6 +1440,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
      * @hide
      */
     @SystemApi
+    @RequiresApi(Build.VERSION_CODES.S)
     public boolean isOemPaid() {
         if (!SdkLevel.isAtLeastS()) {
             throw new UnsupportedOperationException();
@@ -1448,6 +1453,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
      * @hide
      */
     @SystemApi
+    @RequiresApi(Build.VERSION_CODES.S)
     public boolean isOemPrivate() {
         if (!SdkLevel.isAtLeastS()) {
             throw new UnsupportedOperationException();
@@ -1458,6 +1464,7 @@ public final class WifiNetworkSuggestion implements Parcelable {
     /**
      * @see Builder#setCarrierMerged(boolean)
      */
+    @RequiresApi(Build.VERSION_CODES.S)
     public boolean isCarrierMerged() {
         if (!SdkLevel.isAtLeastS()) {
             throw new UnsupportedOperationException();
@@ -1498,15 +1505,13 @@ public final class WifiNetworkSuggestion implements Parcelable {
      */
     @IntRange(from = 0)
     public int getPriorityGroup() {
-        if (!SdkLevel.isAtLeastS()) {
-            throw new UnsupportedOperationException();
-        }
         return priorityGroup;
     }
 
     /**
      * @see Builder#setSubscriptionId(int)
      */
+    @RequiresApi(Build.VERSION_CODES.S)
     public int getSubscriptionId() {
         if (!SdkLevel.isAtLeastS()) {
             throw new UnsupportedOperationException();
@@ -1520,9 +1525,6 @@ public final class WifiNetworkSuggestion implements Parcelable {
      */
     @SystemApi
     public int getCarrierId() {
-        if (!SdkLevel.isAtLeastS()) {
-            throw new UnsupportedOperationException();
-        }
         return wifiConfiguration.carrierId;
     }
 }
