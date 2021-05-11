af94a294dc9138a0b1342d9d73058f03e12437d8
Sunil Ravi <sunilravi@google.com>
2021-05-11 12:23:47 -0700

    Metrics for Steering requests
    Added metrics for total number of steering
    requests received from the AP.
    
    Bug: 181585280
    Test: atest WifiMetricsTest
    Updated-PDD: TRUE
    Change-Id: I16a6f1eeb497c8819d39641e269048263f3b8959

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -6006,6 +6006,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
             return;
         }
 
+        mWifiMetrics.incrementSteeringRequestCount();
+
         if ((frameData.mBssTmDataFlagsMask
                 & MboOceConstants.BTM_DATA_FLAG_MBO_CELL_DATA_CONNECTION_PREFERENCE_INCLUDED)
                 != 0) {
